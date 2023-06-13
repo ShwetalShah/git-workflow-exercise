83347941f7a616ec713e1defcfddf1b4e31b2a01
ShwetalShah <shwetdesai@gmail.com>
2023-06-13 11:18:50 +0100

    committing flow.sh using script in workflow exercise.

--- flow.sh
+++ flow.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # stage changes
-git _
+git add -A
 
 # commit changes
-git _
+git commit -m "committing flow.sh using script in workflow exercise."
 
 # push changes
-git _
+git push origin main
